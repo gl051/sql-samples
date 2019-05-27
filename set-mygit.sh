afbfacf471826e1f742098fd5cd5bfe31e26e5f0
Gianluca <gianlucab@syapse.com>
2019-05-27 12:17:44 -0700

    initial commit

---
+++ set-mygit.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Setting git config user email to gl051@biccari.com"
+git config user.email 'gl051@biccari.com'
+git config user.name 'gl051'
+echo "User email:"
+git config user.email
+echo "User name:"
+git config user.name
